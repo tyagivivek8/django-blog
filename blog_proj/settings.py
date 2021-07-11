e566920dccb5911043f4dc1bfe10fadf70bf2fd0
Vivek Tyagi <f20190627@goa.bits-pilani.ac.in>
2021-07-11 23:46:49 +0530

    Update settings.py

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-382ives1#73kugp8+i8a1g#c48bl7v88)r=5p$7*7dyt6ajze^'
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
